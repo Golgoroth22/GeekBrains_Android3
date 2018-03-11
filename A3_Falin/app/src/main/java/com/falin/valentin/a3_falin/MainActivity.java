9a839141ab6f2dc786edbce6cd4d8af4aa45c1b8
Golgoroth22 <falinv22@gmail.com>
2018-03-11 22:30:24 +0700

    init commit
    Android3

---
+++ A3_Falin/app/src/main/java/com/falin/valentin/a3_falin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.falin.valentin.a3_falin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
