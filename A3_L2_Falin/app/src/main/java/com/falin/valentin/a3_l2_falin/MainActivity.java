1245ee27b360dea034bc4063883fec5e82f391d5
Golgoroth22 <falinv22@gmail.com>
2018-03-16 22:45:34 +0700

    Init lesson2 code. Part 5 ready

---
+++ A3_L2_Falin/app/src/main/java/com/falin/valentin/a3_l2_falin/MainActivity.java
@@ -0,0 +1,36 @@
+package com.falin.valentin.a3_l2_falin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.content_text);
+        editText = findViewById(R.id.content_edit_text);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                textView.setText(editable.toString());
+            }
+        });
+    }
+}
