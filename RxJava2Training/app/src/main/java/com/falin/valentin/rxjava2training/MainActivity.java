4624acafbce152b9f43a8b161cff09f0a1a3532e
Golgoroth22 <falinv22@gmail.com>
2018-04-06 21:56:53 +0700

    init commit for RxJava2 testing

---
+++ RxJava2Training/app/src/main/java/com/falin/valentin/rxjava2training/MainActivity.java
@@ -0,0 +1,13 @@
+package com.falin.valentin.rxjava2training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
