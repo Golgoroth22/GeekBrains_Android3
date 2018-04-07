f30f64525c7a20756ceceb60b7f31f23c205909a
Golgoroth22 <falinv22@gmail.com>
2018-04-07 22:49:13 +0700

    Add listeners to second and third button. Create presenter and model. Test some observers

---
+++ RxJava2Training/app/src/main/java/com/falin/valentin/rxjava2training/presenter/Presenter.java
@@ -0,0 +1,36 @@
+package com.falin.valentin.rxjava2training.presenter;
+
+import android.view.View;
+
+import com.falin.valentin.rxjava2training.model.Model;
+import com.falin.valentin.rxjava2training.view.ViewFragment;
+
+import io.reactivex.Observable;
+
+public class Presenter {
+    private Model model;
+    private ViewFragment fragment;
+
+    public Presenter(Model model) {
+        this.model = model;
+    }
+
+    public void attachViewFragment(ViewFragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public void firstButtonClick() {
+        Observable<String> stringObservable = model.getStringObservable();
+        fragment.firstButtonClick(stringObservable);
+    }
+
+    public void secondButtonClick() {
+        Observable<Integer> integerObservable = model.getIntegerObservable();
+        fragment.secondButtonClick(integerObservable);
+    }
+
+    public void thirdButtonClicked() {
+        Observable<Long> longObserver = model.getLongObserver();
+        fragment.thirdButtonClicked(longObserver);
+    }
+}
--- RxJava2Training/app/src/main/java/com/falin/valentin/rxjava2training/view/ViewFragment.java
+++ RxJava2Training/app/src/main/java/com/falin/valentin/rxjava2training/view/ViewFragment.java
@@ -10,10 +10,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.falin.valentin.rxjava2training.R;
+import com.falin.valentin.rxjava2training.model.Model;
+import com.falin.valentin.rxjava2training.presenter.Presenter;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 
 public class ViewFragment extends Fragment {
@@ -23,8 +27,13 @@ public class ViewFragment extends Fragment {
     TextView textView;
     ImageView imageView;
 
+    Presenter presenter;
+
 
     public ViewFragment() {
+        Model model = new Model();
+        presenter = new Presenter(model);
+        presenter.attachViewFragment(this);
     }
 
 
@@ -41,17 +50,33 @@ public class ViewFragment extends Fragment {
         firstButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firstButtonClick();
+                presenter.firstButtonClick();
             }
         });
         secondButton = view.findViewById(R.id.second_button);
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.secondButtonClick();
+            }
+        });
         thirdButton = view.findViewById(R.id.third_button);
+        thirdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                presenter.thirdButtonClicked();
+            }
+        });
         textView = view.findViewById(R.id.text);
         imageView = view.findViewById(R.id.image);
     }
 
-    private void firstButtonClick() {
-        Observable<String> observable = Observable.fromArray("One", "Two", "Three");
+    private void clearTextView() {
+        textView.setText("");
+    }
+
+    public void firstButtonClick(Observable<String> stringObservable) {
+        clearTextView();
         Observer<String> observer = new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -73,6 +98,60 @@ public class ViewFragment extends Fragment {
                 textView.append("- onComplete method.");
             }
         };
-        observable.subscribe(observer);
+        stringObservable.subscribe(observer);
+    }
+
+    public void secondButtonClick(Observable<Integer> integerObservable) {
+        clearTextView();
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                textView.append("- onSubscribe method " + d + "\n");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                textView.append("- onNext method " + integer + "\n");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append("- onError method " + e + "\n");
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append("- onComplete method.");
+            }
+        };
+        integerObservable.subscribe(observer);
+    }
+
+    public void thirdButtonClicked(Observable<Long> longObserver) {
+        clearTextView();
+        Observer<Long> observer = new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                textView.append("- onSubscribe method " + d + "\n");
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                textView.append("- onNext method " + aLong + "\n");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append("- onError method " + e + "\n");
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append("- onComplete method.");
+            }
+        };
+        longObserver.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(observer);
     }
 }
